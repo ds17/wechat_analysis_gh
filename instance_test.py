a4a5f42e2e3e340657a8eb9ec717ababb0e57e4d
ds17 <ingsed@outlook.com>
2017-03-02 22:55:58 +0800

    20170302_2255

--- instance_test.py
+++ instance_test.py
@@ -1,7 +1,15 @@
 #D:\Python\Python35\python
 # -*- coding:utf-8 -*-
 
-import logging,os
-log1=logging.getLogger('log1')
-formatter1=logging.Formatter('%(asctime)s')
+import requests
 
+url='https://www.google.com.hk'
+proxy={'https':'https://127.0.0.1:8087','http': 'http://127.0.0.1:8087'}
+
+try:
+
+    r=requests.get(url,proxies=proxy)
+    r.raise_for_status()
+    print(r.status_code)
+except:
+    print('error')
