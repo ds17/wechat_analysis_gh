dc7509fe608b8bd03788701e3bcde26606fff2f5
ds17 <ingsed@outlook.com>
2017-03-04 09:08:06 +0800

    Update instance_test.py

--- instance_test.py
+++ instance_test.py
@@ -2,14 +2,9 @@
 # -*- coding:utf-8 -*-
 
 import requests
-
-url='https://www.google.com.hk'
-proxy={'https':'https://127.0.0.1:8087','http': 'http://127.0.0.1:8087'}
-
-try:
-
-    r=requests.get(url,proxies=proxy)
-    r.raise_for_status()
-    print(r.status_code)
-except:
-    print('error')
+api='http://m.weibo.cn/index/my?format=cards&page=%s'
+i=1
+url=api % i
+r=requests.get(url)
+data=r.json()
+print(data)
